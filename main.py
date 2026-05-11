b3e0fd3e487892e1aefda5eacc9ac387d97768ae
4erryMyxa <krivecmaksim9@gmail.com>
2026-05-11 18:54:53 +0300

    Initial commit: Python learning files

---
+++ main.py
@@ -0,0 +1,21 @@
+import json
+def save_report(users):
+     total_age = 0
+     for user in users:
+         total_age = total_age + user["age"]  # складываем все возрасты
+     average_age = total_age / len(users)    # делим на количество  # должно быть 19.5
+     report = {
+           "total_users": len(users),
+           "average_age": average_age,
+           "users": users
+}
+     with open("report.json", "w") as file:
+         json.dump(report, file)
+users = [
+    {"name": "Alex", "age": 15},
+    {"name": "Ilya", "age": 21},
+    {"name": "Anton", "age": 17},
+    {"name": "Max", "age": 25}
+]
+save_report(users)
+print("Готово! Файл сохранён.")
